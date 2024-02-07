51aa873eb1b5f5f63ebb8f07548e9a8654230616
Iván Rodríguez Gómez <85286137+Ivrogo@users.noreply.github.com>
2024-02-07 15:12:09 +0100

    Changes to xslx
    Changed source to xlsx, fixed variables to apply into the template, still gotta fix gmail api con

--- sheets/Scripts/activate.bat
+++ sheets/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Er_jo\OneDrive\Documentos\GitHub\demo-email-template\sheets
+set VIRTUAL_ENV=C:\Workspace\demo-email-template\sheets
 
 if not defined PROMPT set PROMPT=$P$G
 
